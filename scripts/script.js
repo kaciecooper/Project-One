0973de069ff80450f6308e7d3cef7703dff141d6
Kacie Cooper <kcrouch@pdx.edu>
2019-11-25 14:30:44 -0800

    trying to animate waves

--- scripts/script.js
+++ scripts/script.js
@@ -4,34 +4,6 @@ $(function() {
 // Your Code from here on down. Don't delete that line above!
   
 
-var BackgroundScroll = function(params) {
-    params = $.extend({
-        scrollSpeed: 70,
-        imageWidth: $('#bg').width(),
-        imageHeight: $('#bg').height()
-    }, params);
-    
-    var step = 1,
-        current = 0,
-        restartPosition = - (params.imageWidth - params.imageHeight);
-    
-    var scroll = function() {
-        current -= step;
-        if (current == restartPosition){
-            current = 0;
-        }   
-        $('#bg').css('backgroundPosition', current + 'px 0');
-    
-    };
-    
-    this.init = function() {
-        setInterval(scroll, params.scrollSpeed);
-    
-    };
-};
-
-var scroll = new BackgroundScroll();
-scroll.init();
 
 
 
